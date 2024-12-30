92c73757cd3f0da416efd88f1d024ffade04a718
shiven <shiven.career@proton.me>
2024-12-30 14:23:53 +0530

    Feat: setup user permissions

--- profiledef.sh
+++ profiledef.sh
@@ -17,7 +17,8 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
