362c930e7da75da4a0a4a42e1e65f912c5aef106
shiven <shiven.career@proton.me>
2025-01-01 08:35:28 +0530

    Feat: dconf configuration applied successfully

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -6,5 +6,12 @@ systemctl set-default graphical.target
 
 systemctl enable NetworkManager.service
 
-# Update the dconf database with custom settings
+# # Update the dconf database with custom settings
+
+# apply the icon theme.
+dconf compile /etc/dconf/db/local /etc/dconf/db/local.d/
+
+mkdir -p /home/pithuu/.config/dconf/
+dconf compile /home/pithuu/.config/dconf/user /etc/dconf/db/local.d/
+
 dconf update
