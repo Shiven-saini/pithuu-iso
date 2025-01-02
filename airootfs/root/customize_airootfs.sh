c075e4c116d948fba305af5cafe883763cebbcab
shiven <shiven.career@proton.me>
2025-01-02 17:42:45 +0530

    Chore: added the script

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 systemctl enable gdm.service
-
 systemctl set-default graphical.target
-
 systemctl enable NetworkManager.service
+
+restore_files.sh
 
 # Update the dconf database
 dconf compile /etc/dconf/db/local /etc/dconf/db/local.d/
