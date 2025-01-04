baa553c53502df5bbd50809668dcb1cadfc0f0c9
shiven <shiven.career@proton.me>
2025-01-04 18:38:18 +0530

    Fix: update customize script

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -4,7 +4,21 @@ systemctl enable gdm.service
 systemctl set-default graphical.target
 systemctl enable NetworkManager.service
 
-restore_files.sh
+# Copying back the default files
+echo "Restoring /etc/lsb-release..."
+cp /usr/local/share/lsb-release /etc/lsb-release
+
+echo "Restoring /usr/lib/os-release..."
+cp /usr/local/share/os-release /usr/lib/os-release
+
+# Setting correct permissions
+echo "Setting permissions for /etc/lsb-release..."
+chmod 644 /etc/lsb-release
+
+echo "Setting permissions for /usr/lib/os-release..."
+chmod 644 /usr/lib/os-release
+
+echo "Restoration complete!"
 
 # Update the dconf database
 dconf compile /etc/dconf/db/local /etc/dconf/db/local.d/
